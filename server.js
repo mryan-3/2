11b933c1e7c8a3c913902ad815d91e20bc355306
mryan-3 <malawaryan@gmail.com>
2023-09-13 21:47:17 +0300

    changed supabase client

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express()
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(`${supabaseUrl}`, `${supabaseKey}`)
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 app.use(express.json())
 
